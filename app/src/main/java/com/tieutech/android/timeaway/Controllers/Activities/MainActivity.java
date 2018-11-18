ea423c69c7820a213d5cec7ebf8f953e6b22b93d
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-18 20:14:55 +1100

    Set MainActivity to host the TimeFragment

--- app/src/main/java/com/tieutech/android/timeaway/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/tieutech/android/timeaway/Controllers/Activities/MainActivity.java
@@ -1,10 +1,13 @@
-package com.tieutech.android.timeaway;
+package com.tieutech.android.timeaway.Controllers.Activities;
 
 
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.tieutech.android.timeaway.Controllers.Fragments.TimeFragment;
+import com.tieutech.android.timeaway.R;
 
 //Main Actity - contains the fragments of the app
 public class MainActivity extends AppCompatActivity {
@@ -19,7 +22,14 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
 
-        Fragment fragment = fragmentManager.findFragmentById();
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
+
+        if (fragment == null){
+            fragment = new TimeFragment();
+
+            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+        }
+
     }
 
 
