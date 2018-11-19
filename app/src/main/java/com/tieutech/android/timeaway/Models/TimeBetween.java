afefdf2616631f1eae062fd92885eaa1dff110f9
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-19 18:07:42 +1100

    Created TimeBetween class - a model to be passed from the TimeBetweenDetailFragment controller, of which is to be displayed in the TimeBetweenListFragment

---
+++ app/src/main/java/com/tieutech/android/timeaway/Models/TimeBetween.java
@@ -0,0 +1,34 @@
+package com.tieutech.android.timeaway.Models;
+
+import java.util.Date;
+
+public class TimeBetween {
+
+    //========= INSTANCE VARIABLES =========================
+    Date mDateFirst;
+    int mHourFirst;
+    int mMinuteFirst;
+
+
+    Date mDateSecond;
+    int mHourSecond;
+    int mMinuteSecond;
+
+
+
+
+    int mYearDifference;
+    int mMonthDifference;
+    int mWeekDifference;
+    int mDayDifference;
+    int mHourDifference;
+    int mMinuteDifference;
+
+
+
+
+
+
+
+
+}
