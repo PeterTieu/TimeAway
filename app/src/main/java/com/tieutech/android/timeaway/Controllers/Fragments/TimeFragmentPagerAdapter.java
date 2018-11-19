e80a4a673b61906b8f214749e0272ba869ef56ae
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-19 17:46:49 +1100

    Set up methods to open TimeSinceRemainingFragment and TimeBetweenFragment in the TimeFragmentPagerAdapter

--- app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/TimeFragmentPagerAdapter.java
+++ app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/TimeFragmentPagerAdapter.java
@@ -1,4 +1,53 @@
 package com.tieutech.android.timeaway.Controllers.Fragments;
 
-public class TimeFragmentPagerAdapter {
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TimeFragmentPagerAdapter extends FragmentPagerAdapter{
+
+    private static final int FRAGMENT_COUNT = 2;
+
+    public TimeFragmentPagerAdapter(FragmentManager fragmentManager){
+        super(fragmentManager);
+    }
+
+
+    @Override
+    public Fragment getItem(int position){
+
+        switch (position){
+            case 0:
+                return new TimeSinceRemainingFragment();
+
+            case 1:
+                return new TimeBetweenFragment();
+        }
+
+        return null;
+    }
+
+
+
+    @Override
+    public int getCount(){
+        return FRAGMENT_COUNT;
+    }
+
+
+    @Override
+    public CharSequence getPageTitle(int position){
+        switch (position){
+            case 0:
+                return "Time Since/Remaining";
+            case 1:
+                return "Time Between";
+        }
+
+        return null;
+    }
+
+
+
 }
