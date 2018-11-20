29e1d0120a5154b1959b9cec682f83e46f961024
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-20 23:01:57 +1100

    Created TimeBetweenViewPagerActivity - activity to be hosting the TimeBetweenDetailFragment fragments. Will allow for paging between these fragments via horizontal swiping

---
+++ app/src/main/java/com/tieutech/android/timeaway/Controllers/Activities/TimeBetweenViewPagerActivity.java
@@ -0,0 +1,7 @@
+package com.tieutech.android.timeaway.Controllers.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class TimeBetweenViewPagerActivity extends AppCompatActivity{
+
+}
