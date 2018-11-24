20dc301734b1f28f7b6a70de74c93808327d81db
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-24 21:37:03 +1100

    Defined methods in TimeBetweensManager to obtain entire List of TimeBetween objects from the TimeBetween SQLite Database

--- app/src/main/java/com/tieutech/android/timeaway/Databases/TimeBetween/TimeBetweensManager.java
+++ app/src/main/java/com/tieutech/android/timeaway/Databases/TimeBetween/TimeBetweensManager.java
@@ -1,11 +1,15 @@
 package com.tieutech.android.timeaway.Databases.TimeBetween;
 
+import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 
 import com.tieutech.android.timeaway.Models.TimeBetween;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TimeBetweensManager {
 
@@ -63,6 +67,43 @@ public class TimeBetweensManager {
 
 
 
+
+
+    public List<TimeBetween> getTimeBetweens(){
+
+        List<TimeBetween> timeBetweensList = new ArrayList<>();
+
+        TimeBetweenDatabaseCursorWrapper timeBetweenDatabaseCursorWrapper = queryTimeBetweens(null, null);
+
+        try{
+            timeBetweenDatabaseCursorWrapper.moveToFirst();
+
+            while (!timeBetweenDatabaseCursorWrapper.isAfterLast()){
+
+                timeBetweensList.add(timeBetweenDatabaseCursorWrapper.getTimeBetweenFromTimeBetweenDatabase());
+
+                timeBetweenDatabaseCursorWrapper.moveToNext();
+            }
+
+        }
+        finally {
+            timeBetweenDatabaseCursorWrapper.close();
+        }
+
+        return timeBetweensList;
+    }
+
+
+    public void addTimeBetween(TimeBetween timeBetween){
+        ContentValues contentValues = getContentValues(timeBetween);
+        sSQLiteDatabase.insert(TimeBetweenDatabaseSchema.TimeBetweensTable.TIME_BETWEENS_TABLE_NAME, null, contentValues);
+    }
+
+
+
+
+
+
     private TimeBetweenDatabaseCursorWrapper queryTimeBetweens(String whereClause, String[] whereArgs){
         Cursor timeBetweensDatabaseCursor = sSQLiteDatabase.query(
                 TimeBetweenDatabaseSchema.TimeBetweensTable.TIME_BETWEENS_TABLE_NAME,
@@ -78,6 +119,31 @@ public class TimeBetweensManager {
     }
 
 
+
+
+    private static ContentValues getContentValues(TimeBetween timeBetween){
+        ContentValues contentValues = new ContentValues();
+
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.ID, timeBetween.getID().toString());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.TITLE, timeBetween.getTitle());
+
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.DATE_FIRST, timeBetween.getDateFirst().getTime()); //Returns the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this Date object
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.HOUR_FIRST, timeBetween.getHourFirst());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.MINUTE_FIRST, timeBetween.getMinuteFirst());
+
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.DATE_SECOND, timeBetween.getDateSecond().getTime()); //Returns the number of milliseconds since January 1, 1970, 00:00:00 GMT represented by this Date object
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.HOUR_SECOND, timeBetween.getHourSecond());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.MINUTE_SECOND, timeBetween.getMinuteSecond());
+
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.YEAR_DIFFERENCE, timeBetween.getYearDifference());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.MONTH_DIFFERENCE, timeBetween.getMonthDifference());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.WEEK_DIFFERENCE, timeBetween.getWeekDifference());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.DAY_DIFFERENCE, timeBetween.getDayDifference());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.HOUR_DIFFERENCE, timeBetween.getHourDifference());
+        contentValues.put(TimeBetweenDatabaseSchema.TimeBetweensTable.Columns.MINUTE_DIFFERENCE, timeBetween.getMinuteDifference());
+
+        return contentValues;
+    }
 
 
 
