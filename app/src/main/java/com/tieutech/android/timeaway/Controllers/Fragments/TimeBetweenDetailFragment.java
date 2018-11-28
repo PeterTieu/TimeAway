7b1c03da529b644d0e3f6eb85222eb457b2eb543
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-28 00:53:20 -1200

    Overrided onAttach(..) and  onCreate(..) in TimeBetweenDetailFragment

--- app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/TimeBetweenDetailFragment.java
+++ app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/TimeBetweenDetailFragment.java
@@ -1,13 +1,22 @@
 package com.tieutech.android.timeaway.Controllers.Fragments;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
+
+import com.tieutech.android.timeaway.Databases.TimeBetween.TimeBetweensManager;
+import com.tieutech.android.timeaway.Models.TimeBetween;
 
 import java.util.UUID;
 
 public class TimeBetweenDetailFragment extends Fragment{
 
+    private final String TAG = "TBDetailFragment";
     private final static String ARGUMENT_TIME_BETWEEN_ID = "ARGUMENT_TIME_BETWEEN_ID";
+
+    TimeBetween mTimeBetween;
+
 
 
 
@@ -24,6 +33,34 @@ public class TimeBetweenDetailFragment extends Fragment{
         return timeBetweenDetailFragment;
     }
 
+
+
+    //Override onAttach() fragment lifecycle callback method
+    @Override
+    public void onAttach(Context context){
+        super.onAttach(context);
+
+        //Log in Logcat
+        Log.i(TAG, "onAttach() called");
+    }
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+
+        Log.i(TAG, "onCreate(..) called");
+
+        UUID timeBetweenID = (UUID) getArguments().getSerializable(ARGUMENT_TIME_BETWEEN_ID);
+
+        mTimeBetween = new TimeBetween();
+
+        mTimeBetween = TimeBetweensManager.get(getActivity()).getTimeBetween(timeBetweenID);
+
+
+        setHasOptionsMenu(true);
+
+    }
 
 
 
