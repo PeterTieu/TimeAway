764d2b7d5879fec9d389df265e00f06ba1dbddbe
Peter Tieu <tieu.peter.v@gmail.com>
2018-11-28 21:49:18 -1200

    Defined encapsulating constructor in TimeBetweenDetailFragment to begin this fragment from the hosting activity

--- app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/TimeBetweenDetailFragment.java
+++ app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/TimeBetweenDetailFragment.java
@@ -1,4 +1,31 @@
 package com.tieutech.android.timeaway.Controllers.Fragments;
 
-public class TimeBetweenDetailFragment {
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+
+import java.util.UUID;
+
+public class TimeBetweenDetailFragment extends Fragment{
+
+    private final static String ARGUMENT_TIME_BETWEEN_ID = "ARGUMENT_TIME_BETWEEN_ID";
+
+
+
+    public static TimeBetweenDetailFragment newInstance(UUID timeBetweenID){
+
+        Bundle argumentBundle = new Bundle();
+
+        argumentBundle.putSerializable(ARGUMENT_TIME_BETWEEN_ID, timeBetweenID);
+
+        TimeBetweenDetailFragment timeBetweenDetailFragment = new TimeBetweenDetailFragment();
+
+        timeBetweenDetailFragment.setArguments(argumentBundle);
+
+        return timeBetweenDetailFragment;
+    }
+
+
+
+
+
 }
