1eb13c85a92e9e09d8b507880d655fceeffed897
Peter Tieu <tieu.peter.v@gmail.com>
2018-12-04 22:27:46 +1100

    Documented MainActivity

--- app/src/main/java/com/tieutech/android/timeaway/Controllers/Activities/MainActivity.java
+++ app/src/main/java/com/tieutech/android/timeaway/Controllers/Activities/MainActivity.java
@@ -1,6 +1,5 @@
 package com.tieutech.android.timeaway.Controllers.Activities;
 
-
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
@@ -8,6 +7,7 @@ import android.os.Bundle;
 
 import com.tieutech.android.timeaway.Controllers.Fragments.TimeFragment;
 import com.tieutech.android.timeaway.R;
+
 
 //Main Actity - contains the fragments of the app
 public class MainActivity extends AppCompatActivity {
@@ -18,19 +18,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main); //Set layout for the MainActivity
+        FragmentManager fragmentManager = getSupportFragmentManager(); //Create the FragmentManager for interacting with fragments associated with MainActivity
+        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container); //Instantiate Fragment and assign it to associated resource ID
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-
-        Fragment fragment = fragmentManager.findFragmentById(R.id.fragment_container);
-
+        //If the Fragment object DOES NOT exist
         if (fragment == null){
-            fragment = new TimeFragment();
-
-            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit();
+            fragment = new TimeFragment(); //Create a TimeFragment object
+            fragmentManager.beginTransaction().add(R.id.fragment_container, fragment).commit(); //Begin the hosting of the TimeFragment fragment by the MainActivity activity (via the FragmentManager)
         }
 
     }
-
 
 }
