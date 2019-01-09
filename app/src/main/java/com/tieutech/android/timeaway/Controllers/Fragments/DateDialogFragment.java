ac104214a0babfd50aa1d8e52276954c235cb53d
Peter Tieu <tieu.peter.v@gmail.com>
2019-01-09 21:24:15 +1100

    Created constructor for DateDialogFragment class

--- app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/DateDialogFragment.java
+++ app/src/main/java/com/tieutech/android/timeaway/Controllers/Fragments/DateDialogFragment.java
@@ -1,4 +1,37 @@
 package com.tieutech.android.timeaway.Controllers.Fragments;
 
-public class DateDialogFragment {
+import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
+import android.widget.DatePicker;
+
+import java.util.Date;
+
+//DialogFragment fgor DatePicker dialog
+public class DateDialogFragment extends DialogFragment {
+
+
+    //Declare 'key'
+    private static final String ARG_PIX_DATE = "pixDate";
+
+    //Define identifier for dialog fragment extra
+    public static final String EXTRA_DATE = "com.petertieu.android.mepix";
+
+    //Declare layout View of the dialog
+    DatePicker mDatePicker;
+
+
+    //Build encapsulating 'constructor'
+    public static DateDialogFragment newInstance(Date pixDate){
+
+        Bundle argumentBundle = new Bundle(); //Create Bundle object (i.e. argument-bundle)
+
+        argumentBundle.putSerializable(ARG_PIX_DATE, pixDate); //Set key-value pairs for argument-bundle
+
+        DateDialogFragment dateDialogFragment = new DateDialogFragment(); //Create DateDialogFragment
+
+        dateDialogFragment.setArguments(argumentBundle); //Set argument-bundle for the PixDetailFragment
+
+        return dateDialogFragment; //Return DateDialogFragment object
+    }
+
 }
