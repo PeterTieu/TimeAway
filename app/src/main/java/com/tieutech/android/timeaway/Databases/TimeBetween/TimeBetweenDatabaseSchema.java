005df8529426f073fac2e710403ad660927c9760
Peter Tieu <tieu.peter.v@gmail.com>
2019-02-01 15:46:23 +1100

    Documented TimeBetweenDatabaseSchema class

--- app/src/main/java/com/tieutech/android/timeaway/Databases/TimeBetween/TimeBetweenDatabaseSchema.java
+++ app/src/main/java/com/tieutech/android/timeaway/Databases/TimeBetween/TimeBetweenDatabaseSchema.java
@@ -12,22 +12,23 @@ public class TimeBetweenDatabaseSchema {
 
         //Inner-inner class that sets names of the columns of the table
         public static final class Columns{
-            public static final String ID = "ID";
-            public static final String TITLE = "TITLE";
+            public static final String ID = "ID"; //Unique UUID of the TimeBetween
+            public static final String TITLE = "TITLE"; //Title of the TimeBetween
 
-            public static final String DATE_FIRST = "DATE_FIRST";
-            public static final String HOUR_FIRST = "HOUR_FIRST";
-            public static final String MINUTE_FIRST = "MINUTE_FIRST";
-            public static final String DATE_SECOND = "DATE_SECOND";
-            public static final String HOUR_SECOND = "HOUR_SECOND";
-            public static final String MINUTE_SECOND = "MINUTE_SECOND";
+            public static final String DATE_FIRST = "DATE_FIRST"; //Date of the FIRST date
+            public static final String HOUR_FIRST = "HOUR_FIRST"; //Hour of the FIRST date
+            public static final String MINUTE_FIRST = "MINUTE_FIRST"; //Minute of the FIRST date
 
-            public static final String YEAR_DIFFERENCE = "YEAR_DIFFERENCE";
-            public static final String MONTH_DIFFERENCE = "MONTH_DIFFERENCE";
-            public static final String WEEK_DIFFERENCE = "WEEK_DIFFERENCE";
-            public static final String DAY_DIFFERENCE = "DAY_DIFFERENCE";
-            public static final String HOUR_DIFFERENCE = "HOUR_DIFFERENCE";
-            public static final String MINUTE_DIFFERENCE = "MINUTE_DIFFERENCE";
+            public static final String DATE_SECOND = "DATE_SECOND"; //Date of the SECOND date
+            public static final String HOUR_SECOND = "HOUR_SECOND"; //Hour of the SECOND date
+            public static final String MINUTE_SECOND = "MINUTE_SECOND"; //Minute of the SECOND date
+
+            public static final String YEAR_DIFFERENCE = "YEAR_DIFFERENCE"; //DIFFERENCE in Year
+            public static final String MONTH_DIFFERENCE = "MONTH_DIFFERENCE"; //DIFFERENCE in Month
+            public static final String WEEK_DIFFERENCE = "WEEK_DIFFERENCE"; //DIFFERENCE in Week
+            public static final String DAY_DIFFERENCE = "DAY_DIFFERENCE"; //DIFFERENCE in Day
+            public static final String HOUR_DIFFERENCE = "HOUR_DIFFERENCE"; //DIFFERENCE in Hour
+            public static final String MINUTE_DIFFERENCE = "MINUTE_DIFFERENCE"; //DIFFERENCE in Minute
         }
     }
 
